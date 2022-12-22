1385833b6d342da3f738539e72fd206121191086
Edward <novumavis@gmail.com>
2022-12-22 16:05:19 +0600

    New Practise Branch

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <DefaultLikes mymsg="Welcome to Vue2023 App"/> -->
+		<DefaultPosts/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import DefaultLikes from '@/components/DefaultLikes.vue'
+import DefaultPosts from '@/components/DefaultPosts.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    // DefaultLikes,
+		DefaultPosts,
   }
 }
 </script>
