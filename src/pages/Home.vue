bc919565e8b73bc36cc58b98dffc73071b9380fe
Edward <novumavis@gmail.com>
2022-12-23 15:37:42 +0600

    clear ready project

---
+++ src/pages/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="@/assets/logo.png">
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import DefaultLikes from '@/components/DefaultLikes.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    // DefaultLikes,
+  }
+}
+</script>
