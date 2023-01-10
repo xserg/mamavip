d68a53084a2e5a9029450ac98fe1d86fcd48a026
Edward <novumavis@gmail.com>
2023-01-10 17:41:02 +0600

    Auth_v1.0

--- src/components/Autoriz.vue
+++ src/components/Autoriz.vue
@@ -22,12 +22,13 @@
 					</label>
 					<label class="inputWrap">
 						<span class="label">Введите пароль</span>
-						<input type="password">
+						<input :type="inputPassType">
+						<button class="theButton buttonShowPass" :class="{ active: this.inputPassType == 'text' }" @click="showPass"></button>
 					</label>
 					<div class="infoWrap">
 						<button class="theButton buttonWhite fontFamilyB">Забыли пароль?</button>
 						<span class="theTitle">Нет аккаунта?</span>
-						<button class="theButton buttonTransparent fontFamilyB">Зарегистрироваться</button>
+						<button class="theButton buttonTransparent fontFamilyB" @click="this.setRegPage()">Зарегистрироваться</button>
 					</div>
 				</div>
 
@@ -40,7 +41,7 @@
 
 <script>
 
-import {mapState} from 'vuex';
+import {mapState, mapMutations} from 'vuex';
 
 export default {
 	name: 'autoriz',
@@ -49,15 +50,39 @@ export default {
 
 	data(){
 		return{
-			loginForm: {
-				email: '',
-				password: '',
-			},
+			curStep: 1,
+			inputPassType: 'password',
+			// loginForm: {
+			// 	email: '',
+			// 	password: '',
+			// },
 		}
 	},
 
 	
 	methods:{
+		...mapMutations({
+			setLogPage: 'setLogPage',
+			setAuthIn: 'setAuthIn',
+			setAuthOut: 'setAuthOut',
+			setRegPage: 'setRegPage',
+			// hiddenPopup: state => state.hiddenPopup, // какой-то старый не рабочий вариант подключения мутаций из vuex
+		}),
+
+		nextStep(){
+			this.curStep += 1;
+		},
+		prevStep(){
+			this.curStep -= 1;
+		},
+
+		showPass(){
+			if( this.inputPassType == 'password' ){
+				this.inputPassType = 'text';
+			}else{
+				this.inputPassType = 'password';
+			}
+		},
 
 	},
 
@@ -77,6 +102,8 @@ export default {
 <style lang="scss" scoped>
 
 .topLine{
+	background-color: #ffeaeba8;
+  backdrop-filter: blur(5px);
 	position: fixed;
 	width: 100%;
 	top: 0;
@@ -84,6 +111,7 @@ export default {
 	right: 0;
 	padding: 0 16px;
 	justify-content: space-between;
+	z-index: 100;
 	.theButton{
 		width: 100%;
 		max-width: max-content;
@@ -109,6 +137,8 @@ export default {
 	position: relative;
 	z-index: 10;
 	width: 100%;
+	padding-top: 80px;
+	padding-bottom: 40px;
 	.titleLine{
 		margin-bottom: 32px;
 	}
@@ -117,7 +147,7 @@ export default {
 
 
 .formWrap{
-	max-width: 598px;
+	max-width: 568px;
 	margin: 0 auto;
 	width: 100%;
 	
@@ -127,25 +157,79 @@ export default {
 		display: flex;
 		flex-direction: column;
 		align-items: flex-start;
+		position: relative;
 		.label{
 			font-size: 12px;
 			margin-bottom: 4px;
 		}
 		input{
+			position: relative;
+			z-index: 5;
 			width: 100%;
 			border: none;
 			padding: 12px 16px;
 			box-shadow: 0px 16px 20px -8px rgba(253, 124, 132, 0.2);
 			border-radius: 8px;
 			border: 2px solid #FFF;
-			transition: all .24s ease;
+			transition: all .24s ease, letter-spacing .0s ease;
 			outline: none;
+			letter-spacing: 1px;
+			&[type=text]{
+				letter-spacing: 0.6px;
+			}
+			&[type=password]{
+				letter-spacing: 5px;
+			}
 			&::placeholder{
 				color: #23292D4D;
 			}
 			&:focus, &:focus-within, &:active, &:focus-visible, &:target{
 				border: 2px solid #FEABB0;
+				transition: all .24s ease, letter-spacing .0s ease;
+			}
+		}
+		.theButton.buttonShowPass{
+			position: absolute;
+			bottom: 6px;
+			right: 8px;
+			display: block;
+			border-radius: 50%;
+			width: 32px;
+			height: 32px;
+			z-index: 10;
+			&::before{
+				content: '';
+				width: 100%;
+				height: 100%;
+				top: 0;
+				left: 0;
+				position: absolute;
+				background-image: url('../assets/icons/eye-on.svg');
+				background-size: 60%;
+				background-repeat: no-repeat;
+				background-position: center;
+				opacity: 0;
 				transition: all .24s ease;
+			}
+			&::after{
+				content: '';
+				width: 100%;
+				height: 100%;
+				top: 0;
+				left: 0;
+				position: absolute;
+				background-image: url('../assets/icons/eye-off.svg');
+				background-size: 60%;
+				background-repeat: no-repeat;
+				background-position: center;
+				opacity: 1;
+				transition: all .24s ease;
+			}
+			&.active::before{
+				opacity: 1;
+			}
+			&.active::after{
+				opacity: 0;
 			}
 		}
 	}
@@ -167,6 +251,13 @@ export default {
 		}
 	}
 	
+}
+
+
+@media screen and (max-width: 600px) {
+	.stepRegSecond{
+		margin-bottom: 0px;
+	}
 }
 
 
